d615716c33afcf948e21e8da33d1993fabed9e10
prashanth049 <71222337+prashanth049@users.noreply.github.com>
2022-09-16 19:13:05 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,24 +1,10 @@
 import random
 import string
 
-def stretch(text,maxlength):
-    if len(text) < maxlength:
-        randomChar = get_random_char()
-        return stretch(text+randomChar,maxlength)
-    else:
-        return text
+total = string.ascii_letters + string.digits + string.punctuation
 
-def get_random_char():
-    chars = string.printable
-    randomChar = chars[random.randint(0,len(chars)-1)]
-    return randomChar
+length = 16
 
-while 1:
-    maxlen = input(' [?] Enter a length for your password (e for exit): ')
-    try:
-        maxlength = int(maxlen)
-        print("'",stretch('',maxlength),"'\n")
-    except:
-        if maxlen == 'e':
-            break
-        print('Please Enter an integer')
+password = "".join(random.sample(total, length))
+
+print(password)
